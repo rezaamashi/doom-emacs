6ffedf4538152332852f58cc973ec0b0da4d9162
Reza A'masyi <mnurrreza@gmail.com>
2022-04-10 20:22:43 +0700

    refactor(workflow-var): Create separate file for variable
    In this commit I decided to create separate file for workflow variables, which
    later could be called using `load-file`. This is necessary for setting up a
    proper `org-export-async-init-file` or any possible future separate elisp files.

---
+++ elisp/variables.el
@@ -0,0 +1,35 @@
+;; [[file:../config.org::*Org Mode Features][Org Mode Features:1]]
+  (defvar org_file_dir (concat (getenv "HOME") "/org/"))
+  (defvar org_agenda_dir (concat (getenv "HOME") "/org/Orgzly/"))
+
+;; Org journal weekly review
+  (defvar org_journal_weekly_dir (concat (getenv "HOME") "/org/journal/weekly/"))
+;; Org Mode Features:1 ends here
+
+;; [[file:../config.org::*Finance][Finance:1]]
+(defvar hledger_journal_dir (concat (getenv "HOME") "/org/hledger/"))
+;; Finance:1 ends here
+
+;; [[file:../config.org::*Zettelkasten Setup][Zettelkasten Setup:1]]
+  (defvar org_journal (concat (getenv "HOME") "/org/journal/"))
+  (defvar org_noter (concat (getenv "HOME") "/org/roam/noter"))
+  (defvar org_roam (concat (getenv "HOME") "/org/roam/"))
+  (defvar bib_notes (concat (getenv "HOME") "/org/roam/bib_notes/bib_notes.org"))
+  (defvar bib_files '("~/Documents/Reza/BibTex/Zotero-mylib/CalibreBib.bib"
+                      "~/Documents/Reza/BibTex/Zotero-mylib/Zotero-mylib.bib"))
+  (defvar zot_col (concat (getenv "HOME") "/Documents/Reza/BibTex/Zotero-mylib/files/Input"))
+;; Zettelkasten Setup:1 ends here
+
+;; [[file:../config.org::*Project Directory][Project Directory:1]]
+  (defvar proj_dir (concat (getenv "HOME") "/Documents/Reza/Project"))
+  (defvar proj_file (expand-file-name "projectile/projectile.projects"  doom-private-dir))
+;; Project Directory:1 ends here
+
+;; [[file:../config.org::*Email][Email:1]]
+  (defvar draft_dir (concat (getenv "HOME") "/Maildir/draft"))
+  (defvar gmail_sent_dir (concat (getenv "HOME") "/Maildir/mnurrreza-gmail/Sent Items"))
+  (defvar yahoo_sent_dir (concat (getenv "HOME") "/Maildir/mnurreza-yahoo/Sent"))
+  (defvar uni_sent_dir (concat (getenv "HOME") "/Maildir/mnurrreza-gmail/[acc1].Sent Mail"))
+  (defvar mail_dir (concat (getenv "HOME") "/Maildir/"))
+  (defvar mail_script (concat (getenv "HOME") "/.local/script/email"))
+;; Email:1 ends here
