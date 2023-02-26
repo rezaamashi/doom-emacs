74c751e65b3f2e0f0a49356767f42168d267a745
Reza A'masyi <mnurrreza@gmail.com>
2023-02-26 16:56:19 +0700

    chore(variables.el): Modify `variables` file to comply with workflow

--- elisp/variables.el
+++ elisp/variables.el
@@ -1,9 +1,14 @@
+;; [[file:../config.org::*User Credentials][User Credentials:1]]
+(setq user-full-name "Reza A'masyi"
+      user-mail-address "mnurrreza@gmail.com")
+;; User Credentials:1 ends here
+
 ;; [[file:../config.org::*Org Mode Features][Org Mode Features:1]]
   (defvar org_file_dir (concat (getenv "HOME") "/org/"))
   (defvar org_agenda_dir (concat (getenv "HOME") "/org/Orgzly/"))
 
 ;; Org journal weekly review
-  (defvar org_journal_weekly_dir (concat (getenv "HOME") "/org/journal/weekly/"))
+  (defvar org_journal_weekly_dir (concat (getenv "HOME") "/org/journals/weekly/"))
 ;; Org Mode Features:1 ends here
 
 ;; [[file:../config.org::*Finance][Finance:1]]
@@ -11,7 +16,7 @@
 ;; Finance:1 ends here
 
 ;; [[file:../config.org::*Zettelkasten Setup][Zettelkasten Setup:1]]
-  (defvar org_journal (concat (getenv "HOME") "/org/journal/"))
+  (defvar org_journal (concat (getenv "HOME") "/org/journals/"))
   (defvar org_noter (concat (getenv "HOME") "/org/roam/noter"))
   (defvar org_roam (concat (getenv "HOME") "/org/roam/"))
   (defvar bib_notes (concat (getenv "HOME") "/org/roam/bib_notes/bib_notes.org"))
@@ -26,10 +31,7 @@
 ;; Project Directory:1 ends here
 
 ;; [[file:../config.org::*Email][Email:1]]
-  (defvar draft_dir (concat (getenv "HOME") "/Maildir/draft"))
-  (defvar gmail_sent_dir (concat (getenv "HOME") "/Maildir/mnurrreza-gmail/Sent Items"))
-  (defvar yahoo_sent_dir (concat (getenv "HOME") "/Maildir/mnurreza-yahoo/Sent"))
-  (defvar uni_sent_dir (concat (getenv "HOME") "/Maildir/mnurrreza-gmail/[acc1].Sent Mail"))
+  (defvar draft_dir (concat (getenv "HOME") "/Maildir/draft/"))
   (defvar mail_dir (concat (getenv "HOME") "/Maildir/"))
   (defvar mail_script (concat (getenv "HOME") "/.local/script/email"))
 ;; Email:1 ends here
